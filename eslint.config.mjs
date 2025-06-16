4fcf57424411aa6e5c11636ec51b935d893543fe
Joel Byomuhangi <jbyomuhangi@gmail.com>
2025-06-16 17:42:10 +0800

    Removed boilerplate and set up linting and code formatting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,38 @@
+import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
   baseDirectory: __dirname,
+  recommendedConfig: js.configs.recommended,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.config({
+    extends: [
+      "next/core-web-vitals",
+      "eslint:recommended",
+      "plugin:react/recommended",
+      "plugin:react-hooks/recommended",
+      "eslint-config-prettier",
+    ],
+
+    rules: {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+    },
+
+    env: {
+      browser: true,
+      node: true,
+    },
+  }),
+];
 
 export default eslintConfig;
