b31ae38ea71e0ec0e7d8ac139243e9112a628749
Joel Byomuhangi <jbyomuhangi@gmail.com>
2025-06-17 10:44:38 +0800

    Added redirect to dashboard page

---
+++ src/app/(authenticated)/dashboard/page.js
@@ -0,0 +1,8 @@
+import FlexBox from "@/components/FlexBox";
+import React from "react";
+
+const Page = () => {
+  return <FlexBox>Dashboard Page</FlexBox>;
+};
+
+export default Page;
--- src/app/(unauthenticated)/login/components/SuccessStep.js
+++ src/app/(unauthenticated)/login/components/SuccessStep.js
@@ -2,11 +2,20 @@
 
 import { Box, Button, TextField } from "@mui/material";
 import { useMutation } from "@tanstack/react-query";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { useTimeout, useTimeoutFn } from "react-use";
 
 const SuccessStep = ({ onNext }) => {
+  const router = useRouter();
+
+  useTimeoutFn(() => {
+    router.replace("/dashboard");
+  }, 1000);
+
   return (
     <Box>
+      erre
       <Box
         component="p"
         sx={{
